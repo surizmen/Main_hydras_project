2fb08e20b0e6f8a2b93226eb04a4793b8785d93e
surizmen <karach.kirill@bk.ru>
2019-11-05 18:56:33 +0700

    create_bd_migrations

--- yii-application/console/migrations/m191105_093355_create_review_table.php
+++ yii-application/console/migrations/m191105_093355_create_review_table.php
@@ -42,7 +42,7 @@ class m191105_093355_create_review_table extends Migration
             'CASCADE'
         );
 
-        // creates index for column `author_id`
+        // creates index for column `author_id`м
         $this->createIndex(
             '{{%idx-review-author_id}}',
             '{{%review}}',
