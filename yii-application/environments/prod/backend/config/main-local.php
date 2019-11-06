6ce232f21cd1c77660e54072d5bd579674672d1d
surizmen <karach.kirill@bk.ru>
2019-11-06 10:44:24 +0700

    create_bd_migrations

--- yii-application/environments/prod/backend/config/main-local.php
+++ yii-application/environments/prod/backend/config/main-local.php
@@ -26,17 +26,5 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
 
 return $config;
