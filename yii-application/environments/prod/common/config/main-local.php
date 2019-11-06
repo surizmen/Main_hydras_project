3fd5fb90e8aaa50347d98aca4b8b4e8831dba51a
surizmen <karach.kirill@bk.ru>
2019-11-06 10:41:13 +0700

    create_bd_migrations

--- yii-application/environments/prod/common/config/main-local.php
+++ yii-application/environments/prod/common/config/main-local.php
@@ -1,16 +1,40 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            // advanced_yii2 is the database name
+            'dsn' => 'pgsql:host=localhost;dbname=recommendation',
+            'username' => 'postgres',
+            'password' => 'arts123',
             'charset' => 'utf8',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
+        'request' => [
+            'enableCookieValidation' => true,
+
+            'enableCsrfValidation' => true,
+
+            'cookieValidationKey' => 'sdffs',
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
--- yii-application/environments/prod/frontend/config/main-local.php
+++ yii-application/environments/prod/frontend/config/main-local.php
@@ -1,9 +1,40 @@
 <?php
-return [
+
+$config = [
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            // advanced_yii2 is the database name
+            'dsn' => 'pgsql:host=localhost;dbname=recommendation',
+            'username' => 'postgres',
+            'password' => 'arts123',
+            'charset' => 'utf8',
+        ],
         'request' => [
+            'enableCookieValidation' => true,
+
+            'enableCsrfValidation' => true,
+
+            'cookieValidationKey' => 'sdffs',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
